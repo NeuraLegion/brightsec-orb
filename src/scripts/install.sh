b0a76e42b54df4200b841afd9368eebd9156e493
Bar Hofesh <bar.hofesh@gmail.com>
2023-06-21 13:10:00 +0300

    sudo x2

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-sudo wget https://github.com/NeuraLegion/bright-cli/releases/download/v9.2.0/nexploit-cli-linux-x64 -O /usr/local/bin/bright-cli && chmod +x /usr/local/bin/bright-cli
+sudo wget https://github.com/NeuraLegion/bright-cli/releases/download/v9.2.0/nexploit-cli-linux-x64 -O /usr/local/bin/bright-cli
+sudo chmod +x /usr/local/bin/bright-cli
