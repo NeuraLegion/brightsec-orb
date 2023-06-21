a7e1f1b223c695cf8435b8a47d6ccdcf2c952cff
Bar Hofesh <bar.hofesh@gmail.com>
2023-06-21 13:49:21 +0300

    debug

--- src/scripts/scan.sh
+++ src/scripts/scan.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+echo "Running Bright Security scan"
+echo "Bright Security target: ${BRIGHTSEC_TARGET}"
 bright-cli scan:run --token "${BRIGHTSEC_TOKEN}" --crawler "${BRIGHTSEC_TARGET}" --project "${BRIGHTSEC_PROJECT}" --name "${BRIGHTSEC_NAME}"
