6e34a9c956d1382ceb448b713afc2b2c35094ce1
Bar Hofesh <bar.hofesh@gmail.com>
2023-06-21 13:46:35 +0300

    vars

--- src/scripts/scan.sh
+++ src/scripts/scan.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-bright-cli scan:run --token "$BRIGHTSEC_TOKEN" --crawler "$BRIGHTSEC_TARGET" --project "$BRIGHTSEC_PROJECT" --name "$BRIGHTSEC_NAME"
+bright-cli scan:run --token "${BRIGHTSEC_TOKEN}" --crawler "${BRIGHTSEC_TARGET}" --project "${BRIGHTSEC_PROJECT}" --name "${BRIGHTSEC_NAME}"
